283d0bb0fda6bde7c15801ec66ca28cc87eb2089
Blony Maunela <20114950@tafe.wa.edu.au>
2024-03-20 16:05:57 +0800

    other changes

--- test_functions.py
+++ test_functions.py
@@ -1,7 +1,7 @@
-
+import pytest
 from functions import add, subtract, multiply
 from functions import convert_fahrenheit_to_celsius as f2c
-import pytest
+
 
 def test_add():
     assert add(2, 3) == 5
